21c85bda8bdd2b8fd82f125a9a63b34e18a8e99f
Theo Georges <theo.georges@epsi.fr>
2019-06-28 00:13:41 +0200

    ajout mineur sur formulaires

--- src/components/CreateurQuestion.vue
+++ src/components/CreateurQuestion.vue
@@ -4,12 +4,12 @@
       <v-layout row wrap>
         <v-flex grow pa-2>
           <v-textarea
-            auto-grow
             rows="1"
-            v-model="question"
+            auto-grow
+            v-model="text"
             :color="color"
             label="Titre de la question"
-          ></v-textarea>
+          />
         </v-flex>
         <v-flex shrink pa-2>
           <v-select
@@ -19,9 +19,24 @@
             :prepend-icon="prependIcon"
             :color="color"
             v-model="typeQ"
-          ></v-select>
+          />
         </v-flex>
       </v-layout>
+      <!--Si réponse ouverte, champ de texte désactivé-->
+      <v-text-field v-if="typeQ=='qo'" value="Réponse" disabled/>
+      <!--Si choix multiples, liste de choix à paramétrer-->
+      <div v-if="typeQ=='cm'">
+        <v-layout v-for="choice in choices" :key="choice.label" row>
+          <v-text-field
+            prepend-icon="radio_button_unchecked"
+            :label="choice.label"
+            v-model="choice.text"
+          ></v-text-field>
+          <v-btn flat icon :color="color" @click="choices.splice(choice,1)">
+            <v-icon>close</v-icon>
+          </v-btn>
+        </v-layout>
+      </div>
     </v-card>
   </v-container>
 </template>
@@ -31,8 +46,8 @@ export default {
   props: ["color", "type"],
   data: function() {
     return {
-      question: "",
-      choix: [],
+      text: "",
+      choices: [{ label: "Option 1", text: "aff" }],
       items: [
         { value: "cm", label: "Choix multiples", icon: "radio_button_checked" },
         { value: "qo", label: "Question ouverte", icon: "notes" }
